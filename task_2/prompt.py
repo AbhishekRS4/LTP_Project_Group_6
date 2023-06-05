d9e4d14211cfb73a00076c2c1c354dd87d2c901c
Moerie <mortaza.a.y.l@gmail.com>
2023-06-05 12:40:05 +0200

    added singleshot prompt

---
+++ task_2/prompt.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+LABELS = ['Self-direction thought', 'Self-direction action', 'Stimulation', 'Hedonism', 'Achievement', 'Power dominance', 'Power resources', 'Face', 'Security personal', 'Security societal', 'Tradition',
+          'Conformity rules', 'Conformity interpersonal', 'Humility', 'Benevolence caring', 'Benevolence dependability', 'Universalism concern', 'Universalism nature', 'Universalism tolerance', 'Universalism objectivity']
+PROMPT_FORMATS = ["The argument: '{}' \n. Question: Which value category does the argument belong to? Options: {} \n"]
+
+def single_shot_prompt(df):
+    """Creates a single shot prompt for each argument with the first prompt format"""
+
+    template = PROMPT_FORMATS[0]  # use the first template
+    prompts = [
+        template.format(row['body'], ', '.join(LABELS))
+        for _, row in df.iterrows()
+    ]
+    df['body'] = prompts
+    return df
+
+
+if __name__ == "__main__":
+    # load data
+    df = pd.read_csv('subreddit_threads.csv')
+
+    # create single shot prompt
+    df = single_shot_prompt(df)
+
+    # save data
+    df.to_csv('subreddit_threads.csv', index=False)  
