a4681735802e078815b68945d2ebf21360a2dd23
abhishekrs4 <abhishek.r.satyanarayana.4@gmail.com>
2023-06-06 17:35:36 +0200

    updated to include delta along with the predictions

--- task_2/combine_results.py
+++ task_2/combine_results.py
@@ -44,50 +44,55 @@ def main():
                     #print(df_predictions.shape)
                     #print(df_predictions.head())
 
-                    dataset_path = f'author_datasets_orig_prompt/arguments_{author}/'
-                    dataset = load_from_disk(dataset_path)
-                    #print(len(dataset['test']))
-                    #print(list(dataset['test'][:].values())[1])
-                    list_scores = list(dataset['test'][:].values())[1]
-                    df_predictions["score"] = list_scores
-                    num_samples = len(list_scores)
-                    df_predictions["successful"] = (df_predictions["score"] > 0)
+                    try:
+                        dataset_path = f'author_datasets_orig_prompt/arguments_{author}/'
+                        dataset = load_from_disk(dataset_path)
+                        #print(len(dataset['test']))
+                        #print(list(dataset['test'][:].values())[1])
+                        list_scores = list(dataset['test'][:].values())[1]
+                        list_delta = list(dataset['test'][:].values())[2]
+                        df_predictions["score"] = list_scores
+                        num_samples = len(list_scores)
+                        df_predictions["successful"] = (df_predictions["score"] > 0)
+                        df_predictions["delta"] = list_delta
 
-                    list_top_N = list(reversed(sorted(dict_authors_dom_cat[author].keys())))[:top_N]
-                    #print(list_top_N, num_samples)
-                    list_random_cats_counts = random.choices(list_top_N, k=num_samples)
-                    list_random_cats = [dict_authors_dom_cat[author][count] for count in list_random_cats_counts]
-                    #print(len(list_random_cats))
-                    df_predictions["active_subreddit"] = list_random_cats
+                        list_top_N = list(reversed(sorted(dict_authors_dom_cat[author].keys())))[:top_N]
+                        #print(list_top_N, num_samples)
+                        list_random_cats_counts = random.choices(list_top_N, k=num_samples)
+                        list_random_cats = [dict_authors_dom_cat[author][count] for count in list_random_cats_counts]
+                        #print(len(list_random_cats))
+                        df_predictions["active_subreddit"] = list_random_cats
 
 
-                    list_overall_values = []
-                    list_int_labels = df_predictions["pred_int_labels"].to_list()
-                    #print(len(list_int_labels))
-                    for int_values_as_char in list_int_labels:
-                        list_string_values = ""
-                        int_values_as_char = int_values_as_char[1:-1].split(".")[:-1]
+                        list_overall_values = []
+                        list_int_labels = df_predictions["pred_int_labels"].to_list()
+                        #print(len(list_int_labels))
+                        for int_values_as_char in list_int_labels:
+                            list_string_values = ""
+                            int_values_as_char = int_values_as_char[1:-1].split(".")[:-1]
 
-                        int_values = []
-                        for x in int_values_as_char:
-                            int_values.append(int(x.strip()))
-                        int_value_indices = np.nonzero(int_values)[0]
-                        #print(int_value_indices)
-                        for index in int_value_indices:
-                            if list_string_values != "":
-                                list_string_values = list_string_values + "," + list_true_labels[index]
-                            else:
-                                list_string_values = list_true_labels[index]
-                        list_overall_values.append(list_string_values)
+                            int_values = []
+                            for x in int_values_as_char:
+                                int_values.append(int(x.strip()))
+                            int_value_indices = np.nonzero(int_values)[0]
+                            #print(int_value_indices)
+                            for index in int_value_indices:
+                                if list_string_values != "":
+                                    list_string_values = list_string_values + "," + list_true_labels[index]
+                                else:
+                                    list_string_values = list_true_labels[index]
+                            list_overall_values.append(list_string_values)
 
-                    df_predictions["values"] = list_overall_values
+                        df_predictions["values"] = list_overall_values
 
-                    if df_results is None:
-                        df_results = df_predictions.copy()
-                    else:
-                        df_results = pd.concat([df_results, df_predictions], ignore_index=True)
+                        if df_results is None:
+                            df_results = df_predictions.copy()
+                        else:
+                            df_results = pd.concat([df_results, df_predictions], ignore_index=True)
 
-                    print(df_results.shape)
+                        print(df_results.shape)
+                    except:
+                        print("mismatch in samples")
 
     print(df_results.head())
     df_results.to_csv("final_results.csv", index=False)
